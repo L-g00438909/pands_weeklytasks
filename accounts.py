989b643126b9429232db729db8d419887688f6d5
Louise Ryan <louisearyan30@gmail.com>
2024-02-27 16:40:51 +0000

    Week 03 updated

--- accounts.py
+++ accounts.py
@@ -7,7 +7,7 @@ numberentered = False
 while numberentered == False:       # while loop to keep prompting user to enter 10 digits
     accountnumber = input("Please enter a 10 digit account number: ")  # Reads in a 10 digit number from the user
     if len(accountnumber) == 10:    # if else checks if the correct number of digits are entered                                     
-         newaccountnumber = "x" *6 + accountnumber[-4:]    # last 4 characters are sliced/taken out from the end of the string-->7890
+         newaccountnumber = "x" *6 + accountnumber[-4:]    # last 4 characters are sliced/taken out from the end of the string
          print("Your account number is", newaccountnumber)
          numberentered = True       # exit the while loop to prevent an infinite loop
     else:
