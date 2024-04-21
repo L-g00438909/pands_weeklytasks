f085f8505d715523d0f5ad6b43c1291218767c0e
Louise <louisearyan16@gmail.com>
2024-04-21 05:43:31 +0100

    updated reference

--- collatz.py
+++ collatz.py
@@ -3,7 +3,7 @@
 # if even the value is divided by 2, if odd the value is multiplied by 3 and add one. 
 # collatz program ends when the value 1 is reached
 # Author: Louise Ryan
-# Reference: https://www.sanfoundry.com/python-program-test-collatz-conjecture-given-number/
+
 
 
 number = int(input("Please enter a positive integer:"))
@@ -15,3 +15,9 @@ while number != 1:                          # while loop as long as number is no
     else: 
         number = 3*number + 1               # odd number, therefore multiply by 3 and add 1
 print(number)                                  
+
+
+
+
+# References: 
+# https://www.sanfoundry.com/python-program-test-collatz-conjecture-given-number/
